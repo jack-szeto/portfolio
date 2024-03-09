819f6de58b01553b132049d726145016e3632ea2
Jack Szeto <47553851+jack-szeto@users.noreply.github.com>
2024-03-10 06:12:47 +0800

    Remove unused imports from project view component

--- src/app/(project)/projects/[pid]/project.view.tsx
+++ src/app/(project)/projects/[pid]/project.view.tsx
@@ -4,8 +4,6 @@ import {
 	Carousel,
 	CarouselContent,
 	CarouselItem,
-	CarouselNext,
-	CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Project } from "@/data/projects";
 import { cn } from "@/lib/utils";
